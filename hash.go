2f1c752c54bc27ec9fe61408d06a880bbce6b9d7
刘玮 <liuwei4@douyu.tv>
2019-08-14 19:29:06 +0800

    hash

--- hash.go
+++ hash.go
@@ -50,8 +50,8 @@ func HMGet(ctx context.Context, m Model, fields []string, fromCache bool) (notFo
 			return flushHCache(ctx, m)
 		}
 
-		if values != nil && len(values) == 2 {
-			if s, ok := values[0].(string); ok && s == emptyHashValue {
+		if values != nil && len(values) >= 2 {
+			if s, ok := values[0].(string); ok && s == emptyHashField {
 				return true, nil
 			}
 		}
@@ -142,8 +142,8 @@ func BatchHMGet(ctx context.Context, m Model, ids interface{}, fields []string, 
 				continue
 			}
 
-			if values != nil && len(values) == 2 {
-				if s, ok := values[0].(string); ok && s == emptyHashValue {
+			if values != nil && len(values) >= 2 {
+				if s, ok := values[0].(string); ok && s == emptyHashField {
 					continue
 				}
 			}
